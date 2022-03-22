4fbb882af469ea60fbdd7505ca7edbe0a690ac86
4ulepa <xnezerdev@yandex.ru>
2022-03-23 00:19:35 +0300

    Fix some autotest bugs

--- events/urls.py
+++ events/urls.py
@@ -19,6 +19,6 @@ from . import views
 app_name = 'events'
 
 urlpatterns = [
-    path('list', views.event_list, name='event_list'),
-    path('detail/<int:pk>', views.event_detail, name='event_detail')
+    path('list/', views.event_list, name='event_list'),
+    path('detail/<int:pk>/', views.event_detail, name='event_detail')
 ]
